d3d31d5aeab1be59282d947a865a758f8afd096b
mjmccorm <mjmccorm@gmail.com>
2013-04-23 19:53:02 -0400

    Intial project development - no where near complete or even working correctly

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ fire-sim
 ========
 
 Fire Department Simulator
+
+The Fire Department Simulator will allow fire departments to develop training scenarios based on local imagery, maps, and weather conditions.
+
+Smoke, Fire, Electrical, and Liquid spills are types of scenarios that will be supported.
+
+I have a separate project, Mass Casualty Incident Simulator (MCI-Sim), that can be used to train on medical incidents.
---
+++ js/simulations.js
@@ -0,0 +1,151 @@
+var sims = { 
+		"simulations": [
+			{
+			"name": "Simulation 1",
+			"dispatch":"Adams Township Fire...report of a structure fire on Highland St.  There are 2 subjects entrapped.",
+			"fires": [
+				{
+				"fire_id":"0",
+				"xloc":"230",
+				"yloc":"140"
+				},
+				{
+				"fire_id":"1",
+				"xloc":"330",
+				"yloc":"200"
+				}
+				],
+			"images": [
+				{
+				"imageSrc":"simulations/image1.jpg",
+				"caption":"front of house"
+				},
+				{
+				"imageSrc":"simulations/image2.jpg",
+				"caption":"side of house"
+				},
+				{
+				"imageSrc":"simulations/image3.jpg",
+				"caption":"rear of house"
+				}
+				],
+			"map": [
+				{
+				"lat":"47.04197",
+				"lng":"-88.67226"
+				}
+				],
+
+			"resources": [
+				{
+				"members":"15",
+				"mutual-aid":"South Range, Hurontown"
+				}
+				],
+			
+			"water": [
+				{
+				"hydrants":"no hydrants immediately avaiable",
+				"engine1":"E-One has 1200 gal - forgot to fill from last call",
+				"tanker":"3000 gal",
+				"engine2":"Old International - 0 gal available",
+				"engine3":"New International - 500 gal available"
+				}
+				]	
+			},
+			{
+			"name": "Simulation 2",
+			"dispatch":"Adams Township Fire...report of a car fire on Erickson Drive, Atlantic Mine.  No entrapment.",
+			"fires": [
+				{
+				"fire_id":"0",
+				"xloc":"230",
+				"yloc":"140"
+				}
+				],	
+
+			"images": [
+				{
+				"imageSrc":"simulations/image4.png",
+				"caption":"front of car"
+				},
+				{
+				"imageSrc":"simulations/image5.png",
+				"caption":"side of car"
+				},
+				{
+				"imageSrc":"simulations/image6.png",
+				"caption":"rear of car"
+				}],
+			"map": [
+				{
+				"lat":"47.09804",
+				"lng":"-88.62626"
+				}
+				],
+			"resources": [
+				{
+				"members":"15",
+				"mutual-aid":"South Range, Hurontown"
+				}
+				],
+			
+			"water": [
+				{
+				"hydrants":"no hydrants immediately avaiable",
+				"engine1":"E-One has 1200 gal - forgot to fill from last call",
+				"tanker":"3000 gal",
+				"engine2":"Old International - 0 gal available",
+				"engine3":"New International - 500 gal available"
+				}
+				]
+			},
+			{
+			"name": "Simulation 3",
+			"dispatch":"Brush Fire",
+			"fires": [
+				{
+				"fire_id":"0",
+				"xloc":"230",
+				"yloc":"140"
+				}
+				],
+			"images": [
+				{
+				"imageSrc":"simulations/image7.png",
+				"caption":""
+				},
+				{
+				"imageSrc":"simulations/image8.png",
+				"caption":""
+				},
+				{
+				"imageSrc":"simulations/image9.png",
+				"caption":""
+				}],
+			"map":[
+				{
+				"lat":"47.0664",
+				"lng":"-88.6296"
+				}
+				],
+			"resources": [
+				{
+				"members":"15",
+				"mutual-aid":"South Range, Hurontown"
+				}
+				],
+			
+			"water": [
+				{
+				"hydrants":"no hydrants immediately avaiable",
+				"engine1":"E-One has 1200 gal - forgot to fill from last call",
+				"tanker":"3000 gal",
+				"engine2":"Old International - 0 gal available",
+				"engine3":"New International - 500 gal available"
+				}
+				]
+			}
+			
+		]
+	}
